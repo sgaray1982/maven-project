5554dbe4f344da1c3d41d4b6bd85d3fd57af653d
santiago.garay <santiago.garay@globallogic.com>
2018-08-06 17:28:14 -0300

    Fixed Checkstyle warnings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,8 +13,8 @@ public class Greeter {
   }
 
   /**
-   * @param someone
-   * @return
+   * @param someone a string with the name
+   * @return a string with greet message
    */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
